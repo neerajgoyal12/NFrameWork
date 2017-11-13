f822b87df212273f0ded46c76d3a614757a7ead0
Neeraj Goyal <neerajgoyal12@gmail.com>
2017-11-13 23:30:20 +0530

    - added nconfig dummy config

---
+++ NFrameWork/NConfig.h
@@ -0,0 +1,13 @@
+//
+//  NConfig.h
+//  NFrameWork
+//
+//  Created by Systology Software Pvt Ltd on 13/11/17.
+//  Copyright © 2017 Neeraj Goyal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NConfig : NSObject
+
+@end
---
+++ NFrameWork/NConfig.m
@@ -0,0 +1,13 @@
+//
+//  NConfig.m
+//  NFrameWork
+//
+//  Created by Systology Software Pvt Ltd on 13/11/17.
+//  Copyright © 2017 Neeraj Goyal. All rights reserved.
+//
+
+#import "NConfig.h"
+
+@implementation NConfig
+
+@end
