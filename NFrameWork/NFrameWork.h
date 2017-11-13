da0835f5d7aa8900ab2dbb80ac14b35753f3eac1
Neeraj Goyal <neerajgoyal12@gmail.com>
2017-11-13 23:18:44 +0530

    Initial Commit

---
+++ NFrameWork/NFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  NFrameWork.h
+//  NFrameWork
+//
+//  Created by Systology Software Pvt Ltd on 13/11/17.
+//  Copyright © 2017 Neeraj Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NFrameWork.
+FOUNDATION_EXPORT double NFrameWorkVersionNumber;
+
+//! Project version string for NFrameWork.
+FOUNDATION_EXPORT const unsigned char NFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFrameWork/PublicHeader.h>
+
+
